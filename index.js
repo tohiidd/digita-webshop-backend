811c224907daa4881e8c67244f62093574f03945
tohid ebrahimi <tohidebrahimi17@gmail.com>
2023-01-09 11:43:22 +0330

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require("express");
 
 // express app
 const app = express();
-const router = require("./src/routes");
+const router = require("/src/routes");
 
-require("./start/config")(app, express);
-require("./start/db")(app);
-require("./start/logging")();
+require("/start/config")(app, express);
+require("/start/db")(app);
+require("/start/logging")();
 
 // routes
 app.use("/api", router);
